20e33b4931036bf8894193821eb0875b34771f37
Evan Greco <e.greco12@gmail.com>
2025-11-24 11:15:02 -0800

    Init transit-backend

---
+++ src/main/java/com/spezico/transit/TransitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spezico.transit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitBackendApplication.class, args);
+	}
+
+}
